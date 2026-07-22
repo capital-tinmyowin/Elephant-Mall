113bba753f2be46166b1642114b55423fefadebd
SaungThetNgonOo <capital.saungthetngonoo@gmail.com>
2026-07-22 17:42:50 +0630

    update seller info

--- lib/view/product_detail_page.dart
+++ lib/view/product_detail_page.dart
@@ -604,7 +604,7 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
                         child: Text(
                               sellerName,
                               style: const TextStyle(
-                                fontSize: 18,
+                                fontSize: 19,
                                 fontWeight: FontWeight.bold,
                                 color: Colors
                                     .blue, // Optional: makes it look clickable
